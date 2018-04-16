24824d7deb3517ce482bef238c48eed9c233a0ec
strongcode9527 <1477869317@qq.com>
2018-04-16 17:23:28 +0800

    :tada:

---
+++ node/prod.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack'),
+  openBrowser = require('react-dev-utils/openBrowser'),
+  webpackConfig = require('../webpack/webpack.config'),
+  webpackDevServer = require('webpack-dev-server'),
+  chalk = require('chalk'),
+  {
+    choosePort,
+    prepareUrls,
+    createCompiler,
+  } = require('react-dev-utils/WebpackDevServerUtils')
+
+  
+const options = {
+  hot: true,
+  hotOnly: true,
+  contentBase: '/'
+}
+
+
+//node命令行中取得变量，如果不存在，就取默认值。
+const DEFAULT_PORT = +process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+choosePort(HOST, DEFAULT_PORT)
+.then((port) => {
+  //再次加入hmr必要代码。
+  Object.keys(webpackConfig.entry).forEach(item => webpackConfig.entry[item].unshift('react-dev-utils/webpackHotDevClient'))
+  const url = prepareUrls('http', HOST, port),
+    compile = createCompiler(webpack, webpackConfig, 'react-form', url),
+    server = new webpackDevServer(compile, options)
+    server.listen(port, HOST, err => {
+      openBrowser(url.localUrlForBrowser)
+    })
+   
+})
---
+++ webpack/webpack.common.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack'),
+    path = require('path')
+
+module.exports = {
+  entry: '../src/index',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader','eslint-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  }
+}
