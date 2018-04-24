dccdf25d0cc85915c37297b864484d25356fafe3
strongcode9527 <1477869317@qq.com>
2018-04-24 17:17:29 +0800

    :sparkles: tree-shaking 研究

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,15 +2,22 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import asyncComponent from '../components/asyncComponent'
+import {sum} from '../utils'
+import Home from './home'
+import Show from './show'
 
-const AsyncHome = asyncComponent(() => import('./home'))
-const AsyncShow = asyncComponent(() => import('./show'))
+// code-spliting 为了实验tree-shaking先注释。
+// const AsyncHome = asyncComponent(() => import('./home'))
+// const AsyncShow = asyncComponent(() => import('./show'))
+
+// 这里调用函数，只是为了实验tree-shaking
+sum()
 
 const BasicExample = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={AsyncHome} />
-      <Route path="/show" component={AsyncShow} /> 
+      <Route exact path="/" component={Home} />
+      <Route path="/show" component={Show} /> 
     </Switch>
   </Router>
 )
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+export function sum(a, b) {
+  alert('inSumFunction')
+  return a + b
+}
+
+export function subtraction(a, b) {
+  alert('inSubFunction')
+  return a - b
+}
