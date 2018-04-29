b36890a3c75acadf2d474a1b9cf9e5381f28ad24
strongcode9527 <1477869317@qq.com>
2018-04-30 07:18:48 +0800

    :sparkles: class tree-shaking学习完成

--- README.md
+++ README.md
@@ -17,5 +17,24 @@ filename: 'scripts/[name].[hash].bundle.js',
 
 ```
 
+## tree-shaking 学习完成。并且基本能够完成class的tree-shaking。
+
+首先说明原因：class会被babel编译成tree-shaking无法消除的代码。所以我们只有一种解决方式：
+
+**先进行tree-shaking，然后在对bundle进行babel编译。**
 
 
+但是在这里我们也需要注意一个问题。那就是在tree-shaking之前，我们还是需要进行babel的编译的，那就是吧stage-x的代码，编译成标准的js语法。以便于webpack 语法树编译器识别，否则实验性质的js语法，会使得webpack无法识别。
+
+
+
+所以对class进行tree-shaking有四个步骤：
+
+
+1.babel将实验性质的代码，编译成标准代码。对于react技术栈来说第一步需要两个插件。babel-preset-stage-x,以及babel-preset-react
+
+2.webpack，进行tree-shaking标注,并生成最后的bundle.js
+
+3.uglifyjs，进行代码删除和压缩。
+
+4.babel对最后的进行过优化处理后的**标准**js代码，进行兼容性处理，兼容低版本浏览器。这是需要babe-preset-env
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack'),
       merge = require('webpack-merge'),
       common = require('./webpack.common.js'),
-      // UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+      UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
       MinifyPlugin = require('babel-minify-webpack-plugin'),
       BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
       HtmlWebpackPlugin = require('html-webpack-plugin'),
@@ -10,9 +10,7 @@ const webpack = require('webpack'),
 
 module.exports = merge(common, {
   plugins: [
-    new MinifyPlugin({}, {
-      // minifyPreset: env
-    }),
+    new UglifyJSPlugin(),
     new BabelPlugin({
       test: /\.js$/,
       babelOptions: {
